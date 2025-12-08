91f857e827e862d23f89e436533906e2c23dab99
Ririn Indriyani <167736271+RirinIndriyanii@users.noreply.github.com>
2025-12-08 12:16:51 +0700

    Add files via upload

---
+++ Proyek 2/EDA Coffee Shop (1).sql
@@ -0,0 +1,78 @@
+-- 1. Lokasi mana yang memiliki pendapatan terbesar dan berapa jumlah transaksi pada setiap lokasi?
+SELECT store_location lokasi
+      ,ROUND(SUM(sales),2) total_sale
+      ,COUNT(transaction_id) jumlah_transaksi
+
+from `coffee-shop-20231.coffee_shop.transactions-coffeeeshop`
+GROUP BY 1
+ORDER BY 2 DESC;
+
+-- 2. Kapan (jam, hari) yang memiliki pendapatan paling banyak? 
+-- time_period
+SELECT 
+      CASE 
+        WHEN transaction_time BETWEEN '06:00:00' AND '11:59:59' THEN 'Morning'
+        WHEN transaction_time BETWEEN '12:00:00' AND '16:59:59' THEN 'Afternoon'
+        WHEN transaction_time BETWEEN '17:00:00' AND '22:00:00' THEN 'Night'
+        ELSE 'Late Night'
+    END time_period,
+    ROUND(SUM(sales),2) jumlah_sales
+FROM `coffee-shop-20231.coffee_shop.transactions-coffeeeshop`
+GROUP BY 1
+ORDER BY 2 DESC;
+
+
+-- hari (berdasarkan sales)
+SELECT format_date('%A', transaction_date) day_name,
+      ROUND(SUM(sales),2) total_sale
+FROM `coffee-shop-20231.coffee_shop.transactions-coffeeeshop`
+GROUP BY 1
+ORDER BY 2 DESC;
+
+
+-- 3. Produk apa (category, type) yang paling laris dan memiliki kontribusi terbesar terhadap pendapatan?
+-- Top 5 produk category paling laris
+SELECT product_category,
+      SUM(transaction_qty) total_unit
+FROM `coffee-shop-20231.coffee_shop.transactions-coffeeeshop` 
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 5;
+
+-- Top 5 produk category dengan pendapatan terbesar
+SELECT product_category,
+      ROUND(SUM(sales),2) total_sales
+FROM `coffee-shop-20231.coffee_shop.transactions-coffeeeshop` 
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 5;
+
+-- Top 10 produk type paling laris
+SELECT product_type,
+      SUM(transaction_qty) total_unit
+FROM `coffee-shop-20231.coffee_shop.transactions-coffeeeshop` 
+GROUP BY 1
+ORDER BY 2 DESC;
+
+-- Top 10 produk type dengan pendapatan terbesar
+SELECT product_type,
+      ROUND(SUM(sales),2) total_sales
+FROM `coffee-shop-20231.coffee_shop.transactions-coffeeeshop` 
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10;
+
+-- produk type paling laris dan harga
+SELECT product_type,
+      unit_price,
+      SUM(transaction_qty) total_unit
+FROM `coffee-shop-20231.coffee_shop.transactions-coffeeeshop` 
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+
+-- Bagaimana pola penjualan setiap bulan, dan kapan priode puncak penjualan terjadi?
+SELECT FORMAT_DATE('%B', transaction_date) month,
+      ROUND(SUM(sales),2) total_sales
+FROM `coffee-shop-20231.coffee_shop.transactions-coffeeeshop` 
+GROUP BY 1
+ORDER BY 2 DESC;
