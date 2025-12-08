d13613850d717200e993120cadc2fe2883e378f9
Ririn Indriyani <167736271+RirinIndriyanii@users.noreply.github.com>
2025-12-08 13:48:08 +0700

    Update EDA Coffee Shop (1).sql

--- Proyek 2/EDA Coffee Shop (1).sql
+++ Proyek 2/EDA Coffee Shop (1).sql
@@ -1,4 +1,11 @@
--- 1. Lokasi mana yang memiliki pendapatan terbesar dan berapa jumlah transaksi pada setiap lokasi?
+-- Bagaimana pola penjualan setiap bulan, dan kapan priode puncak penjualan terjadi?
+SELECT FORMAT_DATE('%B', transaction_date) month,
+      ROUND(SUM(sales),2) total_sales
+FROM `coffee-shop-20231.coffee_shop.transactions-coffeeeshop` 
+GROUP BY 1
+ORDER BY 2 DESC;
+
+-- Lokasi mana yang memiliki pendapatan terbesar dan berapa jumlah transaksi pada setiap lokasi?
 SELECT store_location lokasi
       ,ROUND(SUM(sales),2) total_sale
       ,COUNT(transaction_id) jumlah_transaksi
@@ -7,7 +14,7 @@ from `coffee-shop-20231.coffee_shop.transactions-coffeeeshop`
 GROUP BY 1
 ORDER BY 2 DESC;
 
--- 2. Kapan (jam, hari) yang memiliki pendapatan paling banyak? 
+-- Kapan periode waktu yang memiliki jumlah transaksi terbanyak? 
 -- time_period
 SELECT 
       CASE 
@@ -16,13 +23,14 @@ SELECT
         WHEN transaction_time BETWEEN '17:00:00' AND '22:00:00' THEN 'Night'
         ELSE 'Late Night'
     END time_period,
-    ROUND(SUM(sales),2) jumlah_sales
+    COUNT(transaction_id) total_transaction,
+    COUNT(transaction_id) / SUM(COUNT(transaction_id)) OVER() * 100 AS percentage_transaction
 FROM `coffee-shop-20231.coffee_shop.transactions-coffeeeshop`
 GROUP BY 1
 ORDER BY 2 DESC;
 
 
--- hari (berdasarkan sales)
+-- Pada hari apa pendapatan tertinggi terjadi?
 SELECT format_date('%A', transaction_date) day_name,
       ROUND(SUM(sales),2) total_sale
 FROM `coffee-shop-20231.coffee_shop.transactions-coffeeeshop`
@@ -30,7 +38,7 @@ GROUP BY 1
 ORDER BY 2 DESC;
 
 
--- 3. Produk apa (category, type) yang paling laris dan memiliki kontribusi terbesar terhadap pendapatan?
+-- Produk apa (category, type) yang paling laris dan memiliki kontribusi terbesar terhadap pendapatan?
 -- Top 5 produk category paling laris
 SELECT product_category,
       SUM(transaction_qty) total_unit
@@ -70,9 +78,3 @@ FROM `coffee-shop-20231.coffee_shop.transactions-coffeeeshop`
 GROUP BY 1, 2
 ORDER BY 3 DESC;
 
--- Bagaimana pola penjualan setiap bulan, dan kapan priode puncak penjualan terjadi?
-SELECT FORMAT_DATE('%B', transaction_date) month,
-      ROUND(SUM(sales),2) total_sales
-FROM `coffee-shop-20231.coffee_shop.transactions-coffeeeshop` 
-GROUP BY 1
-ORDER BY 2 DESC;
